a353ba6c8b4050d70b1c0652b53af06ec000316a
Oscar Widjaja <oscarwdh7@gmail.com>
2023-03-29 17:31:58 +0800

    add working api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 function App() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       {/* <img src={logo} className="App-logo" alt="logo" /> */}
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  // return <div>Map</div>
+  return <Map />;
+}
+
+function Map() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleMap
+      zoom={10}
+      center={{ lat: 44, lng: -80 }}
+      mapContainerClassName="map-container"
+    ></GoogleMap>
   );
 }
 
